950d355c148ceecf5fce8fc3c71aa5da460b23cf
poro832 <ldd826@gmail.com>
2025-09-21 11:12:44 +0900

    리액트연습

---
+++ test/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './LoginForm'; // LoginForm 컴포넌트를 불러옵니다.
+
+function App() {
+  return (
+    // App 컴포넌트 자체에 별도 스타일링이 필요 없습니다.
+    // body 스타일이 LoginForm을 중앙 정렬해줍니다.
+    <LoginForm />
+  );
+}
+
+export default App;

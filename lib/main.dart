ce49b7ff8c41ba97dbed9ebe0ce54cb0c40d8f2f
poro832 <ldd826@gmail.com>
2025-12-03 17:59:48 +0900

    1203 오류

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:dantol_market_app/app.dart';
 import 'package:dantol_market_app/common/controller/authentication_controller.dart';
 import 'package:dantol_market_app/common/controller/bottom_nav_controller.dart';
 import 'package:dantol_market_app/common/controller/data_load_controller.dart';
-import 'package:dantol_market_app/product/page/product_write_page.dart';
+import 'package:dantol_market_app/product/repository/product_repository.dart';
+import 'package:dantol_market_app/product/write/controller/product_write_controller.dart';
+import 'package:dantol_market_app/product/write/page/product_write_page.dart';
 import 'package:dantol_market_app/root.dart';
 import 'package:dantol_market_app/splash/controller/login__controller.dart';
 import 'package:dantol_market_app/splash/controller/splash_controller.dart';
 import 'package:dantol_market_app/user/login/page/login_page.dart';
+import 'package:dantol_market_app/user/model/user_model.dart';
 import 'package:dantol_market_app/user/repository/authentication_repository.dart';
+import 'package:dantol_market_app/user/repository/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -22,6 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: '당근마켓 클론코딩',
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       theme: ThemeData(
         appBarTheme: const AppBarTheme( 
@@ -35,7 +40,11 @@ class MyApp extends StatelessWidget {
       ),
       initialBinding: BindingsBuilder((){
         var authenticationRepository = AuthenticationRepository();
+        var userRepository = UserRepository();
+        var productRepository = ProductRepository();
         Get.put(authenticationRepository);
+        Get.put(userRepository);
+        Get.put(productRepository);
         Get.put(BottomNavController());
         Get.put(SplashController());
         Get.put(DataLoadController());
@@ -48,7 +57,17 @@ class MyApp extends StatelessWidget {
         binding: BindingsBuilder((){
           Get.lazyPut<LoginController>(() => LoginController());
         })),
-        GetPage(name:'/product/write',page:() => const ProductWritePage()),
+        GetPage(
+          name:'/product/write',
+          page:() => ProductWritePage(),
+          binding: BindingsBuilder(() {
+            Get.lazyPut(() => ProductWriteController(
+              Get.find<AuthenticationController>().userModel.value ?? 
+                const UserModel(uid: 'temp_user', name: '임시 사용자'),
+              Get.find<ProductRepository>(),
+            ));
+          }),
+        ),
       ],
     );
   }
